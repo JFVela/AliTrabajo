6b5881749ebbb47ba03a8a2d9f6c92481ee944a5
JFVela <jfvela_2012@hotmail.com>
2024-11-20 20:10:26 -0500

    se corrijio la sintaxis

--- app/views/admin/categorias.php
+++ app/views/admin/categorias.php
@@ -56,52 +56,80 @@
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-                    <form id="formEditarCategoria">
+                    <form id="formEditar">
                         <input type="hidden" id="editarId">
 
-                        <!-- Nombre de la categoría -->
+                        <!-- Nombre -->
                         <div class="mb-3">
-                            <label for="editarNombre" class="form-label gestion-categorias-form-label">Nombre de la categoría</label>
-                            <input type="text" id="editarNombre" class="form-control gestion-categorias-form-input" required
+                            <label for="editarNombre" class="form-label gestion-form-label">Nombre</label>
+                            <input type="text" id="editarNombre" class="form-control gestion-form-input" required
                                 pattern="^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$"
-                                title="El nombre de la categoría no puede contener números ni caracteres especiales"
+                                title="El nombre no puede contener números ni caracteres especiales"
                                 maxlength="100">
                             <small class="form-text text-muted">
                                 Máximo 100 caracteres.
-                                <span id="nombreCategoriaCounter">0/100</span>
+                                <span id="nombreCounter">0/100</span>
                             </small>
                         </div>
 
                         <!-- Descripción -->
                         <div class="mb-3">
-                            <label for="editarDescripcion" class="form-label gestion-categorias-form-label">Descripción</label>
-                            <textarea id="editarDescripcion" class="form-control gestion-categorias-form-input" rows="3"
-                                title="Agregue una descripción para la categoría"></textarea>
+                            <label for="editarDescripcion" class="form-label gestion-form-label">Descripción</label>
+                            <textarea id="editarDescripcion" class="form-control gestion-form-input" rows="3"
+                                title="Agregue una descripción"></textarea>
                         </div>
 
                         <!-- Foto -->
                         <div class="mb-3">
-                            <label for="editarImagen" class="form-label gestion-categorias-form-label">Foto</label>
-                            <input type="file" id="editarImagen" class="form-control gestion-categorias-form-input"
+                            <label for="editarImagen" class="form-label gestion-form-label">Foto</label>
+                            <input type="file" id="editarImagen" class="form-control gestion-form-input"
                                 accept="image/*"
-                                title="Seleccione una imagen para la categoría" required>
+                                title="Seleccione una imagen">
                             <small class="form-text text-muted">Formatos permitidos: JPG, PNG, GIF.</small>
                         </div>
 
+                        <!-- Vista previa de la imagen -->
+                        <div class="mb-3">
+                            <label class="form-label gestion-form-label">Vista previa de la imagen</label>
+                            <div id="imagePreviewContainer" style="display: flex; justify-content: flex-start; align-items: center;">
+                                <img id="imagePreview" src="" alt="Vista previa de la imagen" style="max-width: 200px; display: none; margin-left: 20px;" />
+                            </div>
+                        </div>
+
                         <!-- Botón Actualizar -->
-                        <button type="submit" class="gestion-clientes-boton-modal">
-                            <span id="textoDinamicoCategoria"></span>
+                        <button type="submit" class="gestion-boton-modal">
+                            <span id="textoDinamico"></span>
                         </button>
                     </form>
-
                 </div>
             </div>
         </div>
     </div>
 
     <script>
+        // Mostrar vista previa de la imagen
+        document.getElementById('editarImagen').addEventListener('change', function(event) {
+            const file = event.target.files[0];
+            const imagePreview = document.getElementById('imagePreview');
+
+            if (file) {
+                const reader = new FileReader();
+
+                reader.onload = function(e) {
+                    imagePreview.src = e.target.result;
+                    imagePreview.style.display = 'block'; // Mostrar la imagen
+                };
+
+                reader.readAsDataURL(file);
+            } else {
+                imagePreview.style.display = 'none'; // Ocultar la imagen si no hay archivo
+            }
+        });
+    </script>
+
+    <script>
         //Variables globales
-        let textoModal = document.getElementById("textoDinamicoCategoria");
+        let textoModal = document.getElementById("textoDinamico");
         let tituloModal = document.getElementById("tituloModal");
         // Tabla Ajax
         $(document).ready(function() {
@@ -171,177 +199,176 @@
                     "url": "//cdn.datatables.net/plug-ins/2.1.8/i18n/es-ES.json"
                 }
             });
-        });
+            // Inicializar SweetAlert con botones personalizados
+            const swalWithBootstrapButtons = Swal.mixin({
+                customClass: {
+                    confirmButton: "btn btn-success mx-2",
+                    cancelButton: "btn btn-danger mx-2"
+                },
+                buttonsStyling: false
+            });
 
-        // Inicializar SweetAlert con botones personalizados
-        const swalWithBootstrapButtons = Swal.mixin({
-            customClass: {
-                confirmButton: "btn btn-success mx-2",
-                cancelButton: "btn btn-danger mx-2"
-            },
-            buttonsStyling: false
-        });
+            // Eliminar categoría
+            $('#categoriasTable').on('click', '.eliminar', function() {
+                const id = $(this).data('id');
+                const nombre = $(this).data('nombre');
 
-        // Eliminar categoría
-        $('#categoriasTable').on('click', '.eliminar', function() {
-            const id = $(this).data('id');
-            const nombre = $(this).data('nombre');
-
-            // Confirmar eliminación
-            swalWithBootstrapButtons.fire({
-                title: `¿Está seguro de eliminar la categoría ${nombre}?`,
-                text: "Esta acción no se puede revertir.",
-                icon: "warning",
-                showCancelButton: true,
-                confirmButtonText: "Sí, eliminar",
-                cancelButtonText: "Cancelar",
-                reverseButtons: true
-            }).then((result) => {
-                if (result.isConfirmed) {
-                    // Llamada AJAX para eliminar la categoría
-                    $.post('crudCategorias.php?action=eliminarCategoria', {
-                        id
-                    }, function(response) {
-                        const result = JSON.parse(response);
-                        if (result.success) {
-                            // Mostrar mensaje de éxito
-                            swalWithBootstrapButtons.fire({
-                                title: "¡Eliminado!",
-                                text: `La categoría ${nombre} ha sido eliminada con éxito.`,
-                                icon: "success"
-                            });
-                            table.ajax.reload(); // Recargar la tabla
-                        } else {
-                            // Mostrar mensaje de error con el código o descripción del error
+                // Confirmar eliminación
+                swalWithBootstrapButtons.fire({
+                    title: `¿Está seguro de eliminar la categoría ${nombre}?`,
+                    text: "Esta acción no se puede revertir.",
+                    icon: "warning",
+                    showCancelButton: true,
+                    confirmButtonText: "Sí, eliminar",
+                    cancelButtonText: "Cancelar",
+                    reverseButtons: true
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        // Llamada AJAX para eliminar la categoría
+                        $.post('crudCategorias.php?action=eliminarCategoria', {
+                            id
+                        }, function(response) {
+                            const result = JSON.parse(response);
+                            if (result.success) {
+                                // Mostrar mensaje de éxito
+                                swalWithBootstrapButtons.fire({
+                                    title: "¡Eliminado!",
+                                    text: `La categoría ${nombre} ha sido eliminada con éxito.`,
+                                    icon: "success"
+                                });
+                                table.ajax.reload(); // Recargar la tabla
+                            } else {
+                                // Mostrar mensaje de error con el código o descripción del error
+                                Swal.fire({
+                                    icon: "error",
+                                    title: "Error al eliminar",
+                                    text: result.error
+                                });
+                            }
+                        }).fail(function(jqXHR) {
+                            // Manejar errores de la llamada AJAX
                             Swal.fire({
                                 icon: "error",
-                                title: "Error al eliminar",
-                                text: result.error
+                                title: "Error del servidor",
+                                text: `No se pudo completar la operación. Código de error: ${jqXHR.status}`
                             });
-                        }
-                    }).fail(function(jqXHR) {
-                        // Manejar errores de la llamada AJAX
-                        Swal.fire({
-                            icon: "error",
-                            title: "Error del servidor",
-                            text: `No se pudo completar la operación. Código de error: ${jqXHR.status}`
                         });
-                    });
-                } else if (result.dismiss === Swal.DismissReason.cancel) {
-                    // Cancelación de la acción
-                    swalWithBootstrapButtons.fire({
-                        title: "Cancelado",
-                        text: "La eliminación ha sido cancelada.",
-                        icon: "error"
-                    });
-                }
+                    } else if (result.dismiss === Swal.DismissReason.cancel) {
+                        // Cancelación de la acción
+                        swalWithBootstrapButtons.fire({
+                            title: "Cancelado",
+                            text: "La eliminación ha sido cancelada.",
+                            icon: "error"
+                        });
+                    }
+                });
             });
-        });
 
-        // Editar categoría
-        $('#categoriasTable').on('click', '.editar', function() {
-            tituloModal.textContent = "Editar Categoría";
-            textoModal.textContent = "Editar";
-            const id = $(this).data('id');
-            const nombre = $(this).data('nombre');
-            const descripcion = $(this).data('descripcion');
-            const imagen = $(this).data('imagen');
+            // Editar categoría
+            $('#categoriasTable').on('click', '.editar', function() {
+                tituloModal.textContent = "Editar Categoría";
+                textoModal.textContent = "Editar";
+                const id = $(this).data('id');
+                const nombre = $(this).data('nombre');
+                const descripcion = $(this).data('descripcion');
+                const imagen = $(this).data('imagen');
 
-            // Rellenar los campos del modal
-            $('#editarId').val(id);
-            $('#editarNombre').val(nombre);
-            $('#editarDescripcion').val(descripcion);
-            $('#editarImagen').val(imagen);
+                // Rellenar los campos del modal
+                $('#editarId').val(id);
+                $('#editarNombre').val(nombre);
+                $('#editarDescripcion').val(descripcion);
+                $('#editarImagen').val(imagen);
 
-            // Mostrar el modal
-            $('#modalEditar').modal('show');
-        });
+                // Mostrar el modal
+                $('#modalEditar').modal('show');
+            });
 
-        // Actualizar o Agregar categoría
-        $('#formEditar').submit(function(event) {
-            // Prevenir el comportamiento predeterminado del formulario
-            event.preventDefault();
+            // Actualizar o Agregar categoría
+            $('#formEditar').submit(function(event) {
+                // Prevenir el comportamiento predeterminado del formulario
+                event.preventDefault();
 
-            // Validar formulario
-            if (!this.checkValidity()) {
-                return; // Si el formulario no es válido, no continúa
-            }
+                // Validar formulario
+                if (!this.checkValidity()) {
+                    return; // Si el formulario no es válido, no continúa
+                }
 
-            // Obtener valores del formulario
-            const id = $('#editarId').val();
-            const nombre = $('#editarNombre').val();
-            const descripcion = $('#editarDescripcion').val();
-            const imagen = $('#editarImagen').val();
+                // Obtener valores del formulario
+                const id = $('#editarId').val();
+                const nombre = $('#editarNombre').val();
+                const descripcion = $('#editarDescripcion').val();
+                const imagen = $('#editarImagen').val();
 
-            // Determinar acción según el ID
-            const action = id == 0 ? 'crearCategoria' : 'actualizarCategoria';
+                // Determinar acción según el ID
+                const action = id == 0 ? 'crearCategoria' : 'actualizarCategoria';
 
-            // Crear objeto de datos
-            const datosCategoria = {
-                nombre,
-                descripcion,
-                imagen
-            };
+                // Crear objeto de datos
+                const datosCategoria = {
+                    nombre,
+                    descripcion,
+                    imagen
+                };
 
-            // Si no es una nueva categoría, incluir el ID
-            if (id != 0) {
-                datosCategoria.id = id;
-            }
+                // Si no es una nueva categoría, incluir el ID
+                if (id != 0) {
+                    datosCategoria.id = id;
+                }
 
-            // Enviar datos al servidor
-            $.post(`crudCategorias.php?action=${action}`, datosCategoria, function(response) {
-                // Manejar la respuesta del servidor
-                const result = JSON.parse(response);
+                // Enviar datos al servidor
+                $.post(`crudCategorias.php?action=${action}`, datosCategoria, function(response) {
+                    // Manejar la respuesta del servidor
+                    const result = JSON.parse(response);
 
-                if (result.success) {
-                    // Ocultar el modal y recargar la tabla primero
-                    $('#modalEditar').modal('hide');
-                    table.ajax.reload();
-                    // Mostrar mensaje de éxito después
-                    Swal.fire({
-                        icon: 'success',
-                        title: '¡Éxito!',
-                        text: 'Operación realizada correctamente.',
-                        confirmButtonText: 'Aceptar'
-                    });
-                } else {
-                    // Mostrar mensaje de error
+                    if (result.success) {
+                        // Ocultar el modal y recargar la tabla primero
+                        $('#modalEditar').modal('hide');
+                        table.ajax.reload();
+                        // Mostrar mensaje de éxito después
+                        Swal.fire({
+                            icon: 'success',
+                            title: '¡Éxito!',
+                            text: 'Operación realizada correctamente.',
+                            confirmButtonText: 'Aceptar'
+                        });
+                    } else {
+                        // Mostrar mensaje de error
+                        Swal.fire({
+                            icon: 'error',
+                            title: 'Error',
+                            text: result.error,
+                            confirmButtonText: 'Aceptar'
+                        });
+                    }
+                }).fail(function() {
+                    // Manejo de errores en la solicitud AJAX
                     Swal.fire({
                         icon: 'error',
                         title: 'Error',
-                        text: result.error,
+                        text: 'Ocurrió un error en la comunicación con el servidor.',
                         confirmButtonText: 'Aceptar'
                     });
-                }
-            }).fail(function() {
-                // Manejo de errores en la solicitud AJAX
-                Swal.fire({
-                    icon: 'error',
-                    title: 'Error',
-                    text: 'Ocurrió un error en la comunicación con el servidor.',
-                    confirmButtonText: 'Aceptar'
                 });
             });
-        });
 
-        // Limpiar datos para nueva categoría
-        $('#btnNuevoCliente').click(function() {
-            tituloModal.textContent = "Nueva Categoría";
-            textoModal.textContent = "Agregar Categoría";
-            limpiarDatos();
-            $('#editarId').val(0);
-            $('#modalEditar').modal('show');
-        });
+            // Limpiar datos para nueva categoría
+            $('#btnNuevoCliente').click(function() {
+                tituloModal.textContent = "Nueva Categoría";
+                textoModal.textContent = "Agregar Categoría";
+                limpiarDatos();
+                $('#editarId').val(0);
+                $('#modalEditar').modal('show');
+            });
 
-        function limpiarDatos() {
-            $('#editarId').val("");
-            $('#editarNombre').val("");
-            $('#editarDescripcion').val("");
-            $('#editarImagen').val("");
-        }
+            function limpiarDatos() {
+                $('#editarId').val("");
+                $('#editarNombre').val("");
+                $('#editarDescripcion').val("");
+                $('#editarImagen').val("");
+            }
+
+        });
     </script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-
 
 </body>
 
